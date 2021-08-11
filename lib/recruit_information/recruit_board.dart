01e2259d1fc9ca4fd40548cec317ee1218338f93
To-ToRo <ghkdcks1223@gmail.com>
2021-08-11 19:12:45 +0900

    search bar

--- lib/recruit_information/recruit_board.dart
+++ lib/recruit_information/recruit_board.dart
@@ -15,8 +15,72 @@ class RecruitBoard extends StatelessWidget {
     final recruitController = Get.put(RecruitController());
     // recruitController.getRecrutMain();
 
-    return Container(
-      child: null,
+    final searchText = TextEditingController();
+
+    return RefreshIndicator(
+      onRefresh: recruitController.getRecruitBoard,
+      child: Stack(
+        children: [
+          ListView(),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                SearchBar(searchText: searchText),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// 검색창
+class SearchBar extends StatelessWidget {
+  const SearchBar({
+    Key key,
+    @required this.searchText,
+  }) : super(key: key);
+
+  final TextEditingController searchText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Stack(
+        children: [
+          TextFormField(
+            controller: searchText,
+            textAlign: TextAlign.start,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(100)),
+              ),
+              hintText: 'search',
+              isDense: true,
+              contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+            ),
+            style: TextStyle(),
+          ),
+          Positioned.fill(
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 10),
+                child: Container(
+                    child: InkWell(
+                  onTap: () {
+                    Map arg = {'search': searchText.text, 'from': 'home'};
+                    Get.toNamed('/searchBoard', arguments: arg);
+                  },
+                  child: Icon(Icons.search_outlined),
+                )),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/recruit_information/recruit_state.dart
+++ lib/recruit_information/recruit_state.dart
@@ -6,8 +6,28 @@ import 'package:http/http.dart';
 import 'package:polarstar_flutter/session.dart';
 
 class RecruitController extends GetxController {
-  getRecrutMain() async {
-    var res = await Session().getX('/outside/3/page/1');
+  RxString type = ''.obs, page = ''.obs, bid = ''.obs;
+
+  RxMap recruit_board_body = {}.obs;
+
+  Future<void> getRecruitBoard() async {
+    type.value = '3';
+    page.value = '1';
+
+    Session().getX('/outside/$type/page/$page').then((value) {
+      switch (value.statusCode) {
+        case 200:
+          recruit_board_body.value = json.decode(value.body);
+          break;
+
+        default:
+          recruit_board_body.value = json.decode(value.body);
+      }
+    });
+  }
+
+  Future<void> getRecruitPost() async {
+    var res = await Session().getX('/outside/$type/read/$bid');
     print(json.decode(res.body));
   }
 }
